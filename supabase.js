517c453f3a94fb00e7afc42d5d046fdc8726c867
sihansaniedw26-ux <sihansaniedw26@gmail.com>
2026-09-16 16:12:09 +0530

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://pzhvbzduqauxqneyfoqu.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_cM57HCAfm8treUTx9ifrhA_gkbbAar0";
+
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
